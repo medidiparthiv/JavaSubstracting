2cc8aef2ffbd0ad908441c8330b88d907e498ea1
medidiparthiv <53120768+medidiparthiv@users.noreply.github.com>
2019-07-23 11:44:33 -0400

    Add files via upload

---
+++ Substractions.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner x = new Scanner(System.in);
+	    
+	    System.out.print("Enter First Number : ");
+	    int a = x.nextInt();
+	    
+	    System.out.print("Enter Second Number : ");
+	    int b = x.nextInt();
+	    
+	    System.out.println("The Difference between the Two Numbers is : ");
+	    System.out.println();
+		System.out.println(a-b);
+	}
+}
